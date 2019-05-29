b78f64afc1a61b0ee80a99f96239e0a27e8ea1e8
Anwesh43 <anweshthecool0@gmail.com>
2019-05-30 05:21:49 +0530

    adding constants and libraries

--- boxrotfillview/src/main/java/com/anwesh/uiprojects/boxrotfillview/BoxRotFillView.kt
+++ boxrotfillview/src/main/java/com/anwesh/uiprojects/boxrotfillview/BoxRotFillView.kt
@@ -3,3 +3,23 @@ package com.anwesh.uiprojects.boxrotfillview
 /**
  * Created by anweshmishra on 30/05/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.graphics.RectF
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rotDeg : Float = 90f
+val fillFactor : Float = 0.85f
