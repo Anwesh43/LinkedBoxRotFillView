0f26b6387b01396bc4ea5e759d185c6066a4ff99
Anwesh43 <anweshthecool0@gmail.com>
2019-05-30 06:01:22 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedboxrotfillview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedboxrotfillview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedboxrotfillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.boxrotfillview.BoxRotFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BoxRotFillView.create(this)
     }
 }
